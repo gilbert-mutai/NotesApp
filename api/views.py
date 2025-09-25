7b45499d56dab6a6cd1c0efb15aa62642fb73fe2
Gilbert <gmutai1194@gmail.com>
2025-09-25 23:59:54 +0300

    feat:add a single note view

--- api/views.py
+++ api/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render
 from rest_framework.response import Response
 from rest_framework.decorators import api_view
+from .serializers import NoteSerializer
+from .models import Note
 
 # Create your views here.
 @api_view(['GET'])
@@ -42,4 +44,11 @@ def getRoutes(request):
 @api_view(['GET'])
 def getNotes(request):
     notes = Note.objects.all()
-    return Response("Notes")
+    serializer = NoteSerializer(notes, many=True)
+    return Response(serializer.data)
+
+@api_view(['GET'])
+def getNote(request, pk):
+    notes = Note.objects.get(id=pk)
+    serializer = NoteSerializer(notes, many=False)
+    return Response(serializer.data)
